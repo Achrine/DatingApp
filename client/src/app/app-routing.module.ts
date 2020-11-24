b56e560954368c686ccd6a64b8e4a680098b7ba3
achrine <achrine1@gmail.com>
2020-11-24 06:41:44 -0800

    End of Section 3

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [HttpClientModule, RouterModule.forRoot(routes)],
+
+  exports: [RouterModule],
+
+
+
+})
+export class AppRoutingModule { }
